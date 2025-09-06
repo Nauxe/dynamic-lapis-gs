1ea8673244699bcdfd6855a39de17e48e0f619cb
Yuang <yuangshi@u.nus.edu>
2025-09-06 22:15:21 +0200

    training fullpipeline - checked

--- README.md
+++ README.md
@@ -1,7 +1,50 @@
-# dynamic-lapis-gs
+# Dynamic-LapisGS
 
-The extension of LapisGS to dynamic 3DGS. 
+The extension of LapisGS to Dynamic 3DGS. 
 
-The dynamic LapisGS serves as the foundation of our dynamic 3DGS streaming system, "LTS: A DASH Streaming System for Dynamic Multi-Layer 3D Gaussian Splatting Scenes", which will appear in MMSys'25 in April. Check the [preprint](https://drive.google.com/file/d/1iDz1ExOd1LrPhA7fv4DbLUbzn-Jioihn/view?usp=share_link).
+The Dynamic-LapisGS serves as the foundation of our dynamic 3DGS streaming system, "LTS: A DASH Streaming System for Dynamic Multi-Layer 3D Gaussian Splatting Scenes", which won the **Best Paper Award** at MMSys'25 in April. See the [paper](https://drive.google.com/file/d/1iDz1ExOd1LrPhA7fv4DbLUbzn-Jioihn/view?usp=share_link). 
 
-Will release the code very soon.
+As for the code of LTS system, however, we have received notice from a company asserting that portions of our work may overlap with their patents related to viewport-dependent streaming. Because of this potential legal issue, **we cannot make the code or scripts openly available at this moment**. 
+
+We apologize for any inconvenience this may cause and appreciate your understanding. We remain open to technical discussions and are willing to provide clarifications regarding the implementation through academic or professional exchange.
+
+Luckily, the core code for Dynamic-LapisGS is still available. You can use it to train and render your own dynamic 3DGS models.
+
+# Setup
+
+The code is built on the codebase of [LapisGS](https://github.com/nus-vv-streams/lapis-gs?tab=readme-ov-file), which essentially only modifies a few lines of code to support dynamic 3DGS. Please refer to the LapisGS repository for detailed requirements and elaboration.
+
+# Pre-processing
+
+## Dataset Preparation
+
+
+## Dataset Structure
+
+
+# Running
+
+
+# Citation
+
+If you find our code or paper useful, please cite:
+
+@inproceedings{sun2025lts,
+  author       = {Yuan{-}Chun Sun and
+                  Yuang Shi and
+                  Cheng{-}Tse Lee and
+                  Mufeng Zhu and
+                  Wei Tsang Ooi and
+                  Yao Liu and
+                  Chun{-}Ying Huang and
+                  Cheng{-}Hsin Hsu},
+  title        = {{LTS:} {A} {DASH} Streaming System for Dynamic Multi-Layer {3D} {Gaussian}
+                  Splatting Scenes},
+  booktitle    = {Proceedings of the 16th {ACM} Multimedia Systems Conference, MMSys
+                  2025, Stellenbosch, South Africa, 31 March 2025 - 4 April 2025},
+  pages        = {136--147},
+  publisher    = {{ACM}},
+  year         = {2025},
+  url          = {https://doi.org/10.1145/3712676.3714445},
+  doi          = {10.1145/3712676.3714445},
+}
--- train_full_pipeline.py
+++ train_full_pipeline.py
@@ -4,14 +4,13 @@ from argparse import ArgumentParser
 
 
 if __name__ == "__main__":
-
-    frames_dict = {
-        "longdress": list(range(1051, 1054)),
-        # "longdress": list(range(1051, 1081)),
-        "soldier": list(range(536, 566)),
-        "loot": list(range(1000, 1030)),
-        "redandblack": list(range(1450, 1480)),
-    }
+    
+    # frames_dict = {
+    #     "longdress": list(range(1051, 1081)),
+    #     "soldier": list(range(536, 566)),
+    #     "loot": list(range(1000, 1030)),
+    #     "redandblack": list(range(1450, 1480)),
+    # }
 
     # Set up command line argument parser
     parser = ArgumentParser(description="Training script parameters")
@@ -22,8 +21,8 @@ if __name__ == "__main__":
     parser.add_argument('--method', type=str, required=True, help="Name of the method", choices=["lapis", "freeze", "dynamic-lapis"])
     parser.add_argument('--lambda_dssim', type=float, default=0.8, help="Lambda for DSSIM loss")
     parser.add_argument('--iterations', type=int, default=30_000, help="Number of training iterations for each resolution")
+    parser.add_argument('--frame_list', nargs='+', required=True, help='List of frame numbers to process, pass the start and end frame, e.g., --frame_list 1051 1080')
     args = parser.parse_args(sys.argv[1:])
-
 
     model_base = args.model_base
     dataset_base = args.dataset_base
@@ -32,63 +31,59 @@ if __name__ == "__main__":
     method = args.method
     lambda_dssim = args.lambda_dssim
     iterations = args.iterations
-
+    frame_list = args.frame_list
+    frames_dict = {scene: list(range(int(frame_list[0]), int(frame_list[1])+1))}
     resolution_scales = [8, 4, 2, 1]
-    train_bin = "dynamic-lapis-gs/train.py"
+    train_bin = "train.py"
 
     train_command = ""
 
+    '''
+    Dynamic-LapisGS
+    Combine with the training strategy of "Dynamic 3D Gaussians: Tracking by Persistent Dynamic View Synthesis"
+    Which is,
+    Train the first frame, and only update the position and rotation for the subsequent frames, so that each splat has its correspondence across frames 
+    '''
     if method == "dynamic-lapis":
-        '''Dynamic-LapisGS
-        Combine with the training strategy of "Dynamic 3D Gaussians: Tracking by Persistent Dynamic View Synthesis"
-        Which is,
-        Train the first frame, and only update the position and rotation for the subsequent frames, so that each splat has its correspondence across frames 
-        '''
         first_frame = frames_dict[scene][0]
         subsequent_frames = frames_dict[scene][1:]
 
         # 1. train the first frame from scratch
         for idx, resolution in enumerate(resolution_scales):
-            if not (os.path.isfile(f"{model_base}/{dataset_name}/{scene}/{method}/{scene}_res{resolution}/{first_frame:04}/point_cloud/iteration_30000/point_cloud.ply")): # TEMPORARY, skip the training if the model already exists
+            print(f"Training model for {scene} frame {first_frame:04} at resolution {resolution}")
 
+            model_dir = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"{first_frame:04}")
+            os.makedirs(model_dir, exist_ok=True) # mkdir model_dir if not exists
+            source_dir = os.path.join(dataset_base, dataset_name, scene, f"{scene}_res{resolution}", f"{first_frame:04}")
 
-                print(f"Training model for {scene} frame {first_frame:04} at resolution {resolution}")
+            if resolution == 8: # train from scratch for the first resolution
+                train_command = f"python {train_bin} -s {source_dir} -m {model_dir} --data_device cuda --lambda_dssim {lambda_dssim} --iterations {iterations}"
+            else: # for other resolutions, use the previous resolution's model as foundation GS
+                train_command = f"python {train_bin} -s {source_dir} -m {model_dir} --data_device cuda --lambda_dssim {lambda_dssim}  --iterations {iterations} --dynamic_opacity --foundation_gs_path {model_base}/{dataset_name}/{scene}/{method}/{scene}_res{resolution_scales[idx-1]}/{first_frame:04}/point_cloud/iteration_30000/point_cloud.ply"
 
-                model_dir = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"{first_frame:04}")
-                os.makedirs(model_dir, exist_ok=True) # mkdir model_dir if not exists
-                source_dir = os.path.join(dataset_base, dataset_name, scene, f"{scene}_res{resolution}", f"{first_frame:04}")
-
-                if resolution == 8: # train from scratch for the first resolution
-                    train_command = f"python {train_bin} -s {source_dir} -m {model_dir} --data_device cuda --lambda_dssim {lambda_dssim} --iterations {iterations}"
-                else: # for other resolutions, use the previous resolution's model as foundation GS
-                    train_command = f"python {train_bin} -s {source_dir} -m {model_dir} --data_device cuda --lambda_dssim {lambda_dssim}  --iterations {iterations} --dynamic_opacity --foundation_gs_path {model_base}/{dataset_name}/{scene}/{method}/{scene}_res{resolution_scales[idx-1]}/{first_frame:04}/point_cloud/iteration_30000/point_cloud.ply"
-
-                os.system(train_command) # run the command lines
+            os.system(train_command) # run the command lines
 
 
         # 2. train the subsequent frames based on the previous frame, no densification, only update the position and rotation
         for frame in subsequent_frames:
             for idx, resolution in enumerate(resolution_scales):
-                # if not (os.path.isfile(f"{model_base}/{dataset_name}/{scene}/{method}/{scene}_res{resolution}/dynamic_{frame:04}/point_cloud/iteration_30000/point_cloud.ply")): # TEMPORARY, skip the training if the model already exists
-                if True:
+                print(f"Training model for {scene} frame {frame:04} at resolution {resolution}")
+                
+                model_dir = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"dynamic_{frame:04}")
+                os.makedirs(model_dir, exist_ok=True) # mkdir model_dir if not exists
+                source_dir = os.path.join(dataset_base, dataset_name, scene, f"{scene}_res{resolution}", f"{frame:04}")
+                
 
-                    print(f"Training model for {scene} frame {frame:04} at resolution {resolution}")
-                    
-                    model_dir = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"dynamic_{frame:04}")
-                    os.makedirs(model_dir, exist_ok=True) # mkdir model_dir if not exists
-                    source_dir = os.path.join(dataset_base, dataset_name, scene, f"{scene}_res{resolution}", f"{frame:04}")
-                    
+                if frame == subsequent_frames[0]:
+                    previous_gs_path = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"{(frame-1):04}", "point_cloud", "iteration_30000", "point_cloud.ply")
+                else:
+                    previous_gs_path = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"dynamic_{(frame-1):04}", "point_cloud", "iteration_30000", "point_cloud.ply")
 
-                    if frame == subsequent_frames[0]:
-                        previous_gs_path = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"{(frame-1):04}", "point_cloud", "iteration_30000", "point_cloud.ply")
-                    else:
-                        previous_gs_path = os.path.join(model_base, dataset_name, scene, method, f"{scene}_res{resolution}", f"dynamic_{(frame-1):04}", "point_cloud", "iteration_30000", "point_cloud.ply")
-
-                    # disable densification and opacity reset by:
-                    # 1. setting --densify_from_iter and --opacity_reset_interval larger than number of iterations, 
-                    # 2. setting --densify_until_iter to 0
-                    train_command = f"python {train_bin} -s {source_dir} -m {model_dir} --data_device cuda --lambda_dssim {lambda_dssim}  --iterations {iterations} --densify_from_iter {iterations+1} --densify_until_iter 0 --opacity_reset_interval {iterations+1}  --dynamic_lapis --initial_gs_path {previous_gs_path}"
+                # disable densification and opacity reset by:
+                # 1. setting --densify_from_iter and --opacity_reset_interval larger than number of iterations, 
+                # 2. setting --densify_until_iter to 0
+                train_command = f"python {train_bin} -s {source_dir} -m {model_dir} --data_device cuda --lambda_dssim {lambda_dssim}  --iterations {iterations} --densify_from_iter {iterations+1} --densify_until_iter 0 --opacity_reset_interval {iterations+1}  --dynamic_lapis --initial_gs_path {previous_gs_path}"
 
 
-                    # run the command lines
-                    os.system(train_command)
+                # run the command lines
+                os.system(train_command)
